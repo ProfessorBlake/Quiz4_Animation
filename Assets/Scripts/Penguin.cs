dc8f5cfeec7b1817a5a47819552f5bef42dc9f20
Blake <telefragent@gmail.com>
2020-04-02 10:31:57 -0400

    Initial commit

---
+++ Assets/Scripts/Penguin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penguin : MonoBehaviour
+{
+	public Animator PenguinAnimator;
+	public float WalkSpeed = 1f;
+
+	private void Update()
+	{
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			PenguinAnimator.SetInteger("state", 1);
+			transform.position += new Vector3(-WalkSpeed, 0f, 0f) * Time.deltaTime;
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			PenguinAnimator.SetInteger("state", 2);
+			transform.position += new Vector3(WalkSpeed, 0f, 0f) * Time.deltaTime;
+		}
+		else
+		{
+			PenguinAnimator.SetInteger("state", 0);
+		}
+	}
+}
