b0305159c6379680603aa06084d8a30f295c0595
Blake <telefragent@gmail.com>
2020-04-02 10:34:59 -0400

    Quiz setup

--- Assets/Scripts/Penguin.cs
+++ Assets/Scripts/Penguin.cs
@@ -5,23 +5,10 @@ using UnityEngine;
 public class Penguin : MonoBehaviour
 {
 	public Animator PenguinAnimator;
-	public float WalkSpeed = 1f;
+	public float WalkSpeed = 0.5f;
 
 	private void Update()
 	{
-		if(Input.GetKey(KeyCode.LeftArrow))
-		{
-			PenguinAnimator.SetInteger("state", 1);
-			transform.position += new Vector3(-WalkSpeed, 0f, 0f) * Time.deltaTime;
-		}
-		else if (Input.GetKey(KeyCode.RightArrow))
-		{
-			PenguinAnimator.SetInteger("state", 2);
-			transform.position += new Vector3(WalkSpeed, 0f, 0f) * Time.deltaTime;
-		}
-		else
-		{
-			PenguinAnimator.SetInteger("state", 0);
-		}
+		
 	}
 }
